96c0e208de6123ba5fce56c4819ff4fe3e70b420
enriquito <ssss@sss.com>
2020-04-20 00:48:46 +0200

    Updates

---
+++ precache-manifest.0cf4473d6821db72de0103a8a67ebd2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e74dd85d0e811c87d7db258ad6b0d8f",
+    "url": "/MyFluingNetz/index.html"
+  },
+  {
+    "revision": "fbbc6d6de528adeb7807",
+    "url": "/MyFluingNetz/static/js/2.49d7bce3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/MyFluingNetz/static/js/2.49d7bce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dd6f51beddf9116560",
+    "url": "/MyFluingNetz/static/js/main.dd52a9d6.chunk.js"
+  },
+  {
+    "revision": "3a1b0ebdb075f709e960",
+    "url": "/MyFluingNetz/static/js/runtime-main.fe8ac0c9.js"
+  },
+  {
+    "revision": "3f7ae4b2b9356823825e979036a5f4ec",
+    "url": "/MyFluingNetz/static/media/novipazar.3f7ae4b2.jpg"
+  }
+]);
